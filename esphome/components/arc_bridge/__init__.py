a2a42fc6dd27a8b11651c5bf18080edafcf6fa27
Redstorm1 <redstorm@geektech.co.nz>
2025-11-07 11:35:40 +1300

    fix text sensor

--- esphome/components/arc_bridge/__init__.py
+++ esphome/components/arc_bridge/__init__.py
@@ -1,9 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import cover, uart, sensor
+from esphome.components import cover, uart, sensor, text_sensor
 from esphome.const import CONF_ID, CONF_NAME
 
-DEPENDENCIES = ["uart", "cover", "sensor"]
+DEPENDENCIES = ["uart", "cover", "sensor", "text_sensor"]
 MULTI_CONF = True
 
 CONF_BLINDS = "blinds"
