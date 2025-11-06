503d043f625b2edc3bffb677aca7f4650a02f343
Redstorm1 <redstorm@geektech.co.nz>
2025-11-07 08:13:52 +1300

    removed sensor dependency from import statement

--- components/arc_bridge/__init__.py
+++ components/arc_bridge/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, cover, sensor, text_sensor
+from esphome.components import uart, cover
 from esphome.const import CONF_ID, CONF_NAME
 
 DEPENDENCIES = ["uart"]
