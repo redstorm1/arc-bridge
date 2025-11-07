600b88519de185c57a3d77ec32a18ed6e9b49df2
Redstorm1 <redstorm@geektech.co.nz>
2025-11-08 11:45:59 +1300

    ddd

--- esphome/components/arc_bridge/arc_cover.h
+++ esphome/components/arc_bridge/arc_cover.h
@@ -1,27 +1,34 @@
 #pragma once
-
+#include "esphome/core/component.h"
 #include "esphome/components/cover/cover.h"
-#include <string>
 
 namespace esphome {
 namespace arc_bridge {
 
-class ARCBridgeComponent;
+class ARCBridgeComponent;  // forward declare
 
-class ARCCover : public cover::Cover {
+class ARCCover : public cover::Cover, public Component {
  public:
-  void set_bridge(ARCBridgeComponent *bridge) { bridge_ = bridge; }
-  void set_blind_id(const std::string &id) { this->blind_id_ = id; }
-  const std::string &get_id() const { return id_; }
+  // link to parent bridge
+  void set_bridge(ARCBridgeComponent *bridge) { this->bridge_ = bridge; }
 
+  // assign blind identifier (e.g. "USZ")
+  void set_blind_id(const std::string &id) { this->blind_id_ = id; }
+
+  // store whether HA <-> device mapping should be inverted
+  void set_invert_position(bool invert) { this->invert_position_ = invert; }
+
+  // called by bridge to update position
+  void publish_raw_position(int device_pos);
+
+  // cover interface
   cover::CoverTraits get_traits() override;
   void control(const cover::CoverCall &call) override;
 
-  void publish_raw(int arc_pos);
-
  protected:
   ARCBridgeComponent *bridge_{nullptr};
-  std::string id_;
+  std::string blind_id_;
+  bool invert_position_{false};
 };
 
 }  // namespace arc_bridge
