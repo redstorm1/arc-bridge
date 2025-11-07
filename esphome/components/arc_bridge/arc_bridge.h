5809b7736f2e48041629f3da49d8c9407952170c
Redstorm1 <redstorm@geektech.co.nz>
2025-11-07 13:17:56 +1300

    more fixes

--- esphome/components/arc_bridge/arc_bridge.h
+++ esphome/components/arc_bridge/arc_bridge.h
@@ -58,7 +58,7 @@ class ARCBridgeComponent : public Component, public uart::UARTDevice {
 class ARCBlind : public cover::Cover, public Component {
  public:
   void set_blind_id(const std::string &id) { blind_id_ = id; }
-  void set_name(const std::string &name) { cover::Cover::set_name(name); name_ = name; }
+  void set_name(const std::string &name) { cover::Cover::set_name(name.c_str()); name_ = name; }
   void set_parent(ARCBridgeComponent *parent) { parent_ = parent; }
 
   // lifecycle
