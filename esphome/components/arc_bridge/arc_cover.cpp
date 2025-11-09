692d7b2ca7743daae0deab1fd125d478b10f2717
Redstorm1 <redstorm@geektech.co.nz>
2025-11-10 12:34:25 +1300

    take 10

--- esphome/components/arc_bridge/arc_cover.cpp
+++ esphome/components/arc_bridge/arc_cover.cpp
@@ -34,7 +34,10 @@ void ARCCover::publish_raw_position(int device_pos) {
 
 void ARCCover::publish_unavailable() {
   ESP_LOGW("arc_cover", "[%s] marking as unavailable", this->blind_id_.c_str());
-  this->set_state_unavailable();   // mark the entity itself unavailable in HA
+  // Mark the entity as unavailable using the component status flag
+  this->status_set_warning();      // sets HA entity to unavailable/warning
+  this->position = NAN;            // clear any valid position
+  this->publish_state();           // trigger HA update
 }
 
 void ARCCover::publish_link_quality(float value) {
@@ -46,10 +49,12 @@ void ARCCover::publish_link_quality(float value) {
 
 void ARCCover::set_available(bool available) {
   if (!available) {
-    this->set_state_unavailable();
+    this->status_set_warning();
+    this->position = NAN;
+    this->publish_state();
     ESP_LOGW("arc_cover", "[%s] marked unavailable", this->blind_id_.c_str());
   } else {
-    this->set_state_active();  // re-enable entity
+    this->status_clear_warning();
     if (this->last_known_pos_ >= 0)
       this->publish_raw_position(this->last_known_pos_);
     ESP_LOGD("arc_cover", "[%s] marked available", this->blind_id_.c_str());
